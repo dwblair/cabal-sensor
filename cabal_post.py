356d4aa894bc5a3387f7822511648c6d8133e558
Don Blair <donblair@gmail.com>
2018-11-21 23:48:29 -0500

    update

---
+++ cabal_post.py
@@ -0,0 +1,14 @@
+import subprocess
+from random import randint
+
+
+message='hi hi'
+key='[KEY]'
+channel = 'sensor_test'
+timeout = 1000
+
+while True:
+    
+    message='Temp='+str(randint(0, 100))
+    
+    output=subprocess.check_output(['cabal', '--message',str(message), '--key', str(key), '--channel', str(channel), '--timeout', str(timeout)])
